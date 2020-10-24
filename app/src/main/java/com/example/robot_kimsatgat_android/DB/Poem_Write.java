7584f48fe5f8bc419c359d7c6241ff14b3a6fc7d
KimEuiJoong <jkyj0423@naver.com>
2020-10-24 19:10:56 +0900

    ...

---
+++ app/src/main/java/com/example/robot_kimsatgat_android/DB/Poem_Write.java
@@ -0,0 +1,5 @@
+package com.example.robot_kimsatgat_android.DB;
+
+public class Poem_Write {
+
+}
--- app/src/main/java/com/example/robot_kimsatgat_android/MainActivity.java
+++ app/src/main/java/com/example/robot_kimsatgat_android/MainActivity.java
@@ -2,16 +2,77 @@ package com.example.robot_kimsatgat_android;
 
 import android.app.Activity;
 import android.content.Intent;
+
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.robot_kimsatgat_android.DB.Poem_Write;
+
 import com.example.robot_kimsatgat_android.View.View_LikeList;
 import com.example.robot_kimsatgat_android.View.View_Suggested_Poem;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private AppBarConfiguration mAppBarConfiguration;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.nav_drawer);
+//
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = findViewById(R.id.poem_write);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, Poem_Write.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+//
+//        NavigationView navigationView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        mAppBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+//                .setDrawerLayout(drawer)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView, navController);
+//
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main_settings, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
 
     // 여러 액티비티 한번에 종료하기
     public static Activity _Main_Activity;
@@ -54,13 +115,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
-        
 
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 
 }
