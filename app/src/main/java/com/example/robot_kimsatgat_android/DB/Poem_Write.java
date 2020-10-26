f945cfd5140df27045be6fa40db9c7c20cfe501c
KimEuiJoong <jkyj0423@naver.com>
2020-10-26 17:56:42 +0900

    10.26 수 기본적인 레이아웃 구성 명칭 많이 바뀌었음 꼭 확인 바람

--- app/src/main/java/com/example/robot_kimsatgat_android/DB/Poem_Write.java
+++ app/src/main/java/com/example/robot_kimsatgat_android/DB/Poem_Write.java
@@ -1,5 +1,32 @@
 package com.example.robot_kimsatgat_android.DB;
 
-public class Poem_Write {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.robot_kimsatgat_android.Login.Login;
+import com.example.robot_kimsatgat_android.Questionnaire.Questionnaire1;
+import com.example.robot_kimsatgat_android.R;
+
+public class Poem_Write extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.poem_write);
+
+        Button btn_poem_save= findViewById(R.id.btn_poem_save);
+        btn_poem_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Poem_Write.this, "저장되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 
 }
--- app/src/main/java/com/example/robot_kimsatgat_android/Questionnaire/Questionnaire1.java
+++ app/src/main/java/com/example/robot_kimsatgat_android/Questionnaire/Questionnaire1.java
@@ -1,5 +1,6 @@
 package com.example.robot_kimsatgat_android.Questionnaire;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +14,14 @@ import com.example.robot_kimsatgat_android.View.View_Suggested_Poem;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Questionnaire1 extends AppCompatActivity {
+
+    public static Activity _Questionnaire1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_first);
+        _Questionnaire1 = Questionnaire1.this;
 
         RadioGroup RadioGroup_feeling;
         Button next_to_second_question_button;
