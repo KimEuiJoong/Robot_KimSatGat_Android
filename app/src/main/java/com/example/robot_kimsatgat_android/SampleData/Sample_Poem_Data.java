6750e7990ee7678df143b5922729699e37231d6a
DotG-King <63101027+DotG-King@users.noreply.github.com>
2020-10-28 23:36:47 +0900

    design update

--- app/src/main/java/com/example/robot_kimsatgat_android/SampleData/Sample_Poem_Data.java
+++ app/src/main/java/com/example/robot_kimsatgat_android/SampleData/Sample_Poem_Data.java
@@ -2,22 +2,29 @@
 package com.example.robot_kimsatgat_android.SampleData;
 
 public class Sample_Poem_Data {
-    String text;
+    String main_text;
     String editor;
     String poem_name;
     boolean like;
     String comment;
 
-    public Poem(String poem_name, String editor, String text) {
+    public Poem(String poem_name, String editor, String main_text) {
         this.poem_name = poem_name;
         this.editor = editor;
-        this.text = text;
+        this.main_text = main_text;
     }
 
     public String getPoem_name(){
         return poem_name;
     }
 
-    public String
+    public String getEditor(){
+        return editor;
+    }
+
+    public String getMain_text(){
+        return main_text;
+    }
 }
+
 */
