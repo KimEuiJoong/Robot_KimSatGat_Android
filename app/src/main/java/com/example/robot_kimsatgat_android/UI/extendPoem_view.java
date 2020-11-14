7acd1a72b79d432f4006d15cea69e4a7a980f1fe
DotG-King <63101027+DotG-King@users.noreply.github.com>
2020-11-14 19:42:50 +0900

    1114 rjh

---
+++ app/src/main/java/com/example/robot_kimsatgat_android/UI/extendPoem_view.java
@@ -0,0 +1,39 @@
+package com.example.robot_kimsatgat_android.UI;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.robot_kimsatgat_android.R;
+
+public class extendPoem_view extends Poem_view {
+    EditText comment_edit;
+    ImageButton comment_send;
+    RecyclerView comment_view;
+    View view;
+
+    public extendPoem_view(Context context) {
+        super(context);
+        init(context);
+    }
+    public extendPoem_view(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.extend_poem_view,this,true);
+        poem_title_view = findViewById(R.id.poem_title);
+        poem_writer_view = findViewById(R.id.poem_writer);
+        poem_main_view = findViewById(R.id.poem_main);
+        poem_likenum_view = findViewById(R.id.like_count);
+        Ibtn_poemlike = findViewById(R.id.likeIButton);
+    }
+}
