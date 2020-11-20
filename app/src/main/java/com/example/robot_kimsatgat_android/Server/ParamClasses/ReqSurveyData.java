077f80b67a269d26726f9b10110aa3623131cc19
aprilgom <aprilgom@gmail.com>
2020-11-20 11:49:34 +0900

    ReqSurvey

---
+++ app/src/main/java/com/example/robot_kimsatgat_android/Server/ParamClasses/ReqSurveyData.java
@@ -0,0 +1,9 @@
+package com.example.robot_kimsatgat_android.Server.ParamClasses;
+
+public class ReqSurveyData {
+    String feeling;
+    public ReqSurveyData(String Feeling){
+        feeling = Feeling;
+    }
+
+}
